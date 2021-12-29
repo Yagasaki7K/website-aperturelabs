16d90532ba63ffbbc742daae9dbf38b031fa25cf
Anderson Marlon <anderson18.marlon@gmail.com>
2021-12-29 20:57:32 -0300

    Update links

--- src/components/BestTechs.js
+++ src/components/BestTechs.js
@@ -1,3 +1,7 @@
+function Discord() {
+    window.open("https://discord.gg/nyTRNSV", "_blank")
+}
+
 const BestTechs = () => {
     return (
         <section id="description">
@@ -9,7 +13,7 @@ const BestTechs = () => {
                     <p>No meio de tanta informação e da quantidade de ferramentas que surgem 
                         todos os dias, você precisa de uma comunidade que te leve na direção certa
                     </p>
-                    <button>Quero embarcar nesse foguete</button>
+                    <button onClick={Discord}>Quero entrar nesse servidor</button>
                 </div>
             <img src="/images/developer.svg" alt="Melhores tecnologias" width={300}/>
             </div>
