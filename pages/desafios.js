f1495b3b19b3d35ac9279f29e873008d282ab96f
Anderson Marlon <anderson18.marlon@gmail.com>
2021-12-29 20:24:17 -0300

    Update desafios.js

--- pages/desafios.js
+++ pages/desafios.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
-import Discord from '../src/components/Discord.js'
-import Header from '../src/components/Header.js'
+import Discord from '../components/Discord.js'
+import Header from '../components/Header.js'
 
 const desafios = () => {
     return (
